e01f266114ae00739b1434babcf31285bfe016c7
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-10-21 07:45:28 +0900

    チャート表示
    ライン、エリア、バーチャート追加

--- main.py
+++ main.py
@@ -6,25 +6,12 @@ st.title("Streamlit 超入門")
 
 st.write("DataFrame")
 
-df = pd.DataFrame({
-    "1列目":[1,2,3,4],
-    "2列目":[10,20,30,40]
-})
-#st.write(df)
-#st.dataframe(df.style.highlight_max(axis=0),width=400,height=400) #dataframeの方が使い勝手がいい、動的な表
-st.table(df.style.highlight_max(axis=0)) #静的な表でサイズ指定ができない？
+df = pd.DataFrame(
+    np.random.rand(20,3),
+    columns=["a","b","c"]
+)
 
-"""
-
-# 章
-## 節
-### 項
-
-```python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
-
-"""
-
+#st.table(df.style.highlight_max(axis=0))  ハイライトをつける
+#st.line_chart(df)    ラインチャート
+#st.area_chart(df)    エリアチャート
+#st.bar_chart(df)　　　棒グラフ
