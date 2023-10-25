d9c910e47736373845e393015dd4b52aaca63c04
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-10-25 21:18:20 +0900

    緯度と経度を指定してマップに表示

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ st.title("Streamlit 超入門")
 st.write("DataFrame")
 
 df = pd.DataFrame(
-    np.random.rand(20,3),
-    columns=["a","b","c"]
+    np.random.rand(100,2)/[50,50] + [35.69,139.70],     #NumPyのnp.random.rand()関数を使用して生成されます。この関数は0から1の範囲の一様分布からランダムな数値を生成するもの
+    columns=["lat","lon"]
 )
 
-#st.table(df.style.highlight_max(axis=0))  ハイライトをつける
-#st.line_chart(df)    ラインチャート
-#st.area_chart(df)    エリアチャート
-#st.bar_chart(df)　　　棒グラフ
+st.map(df) #mapを表示
