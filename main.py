51e2d9805d904b2a93267e0758c5e37354b7a574
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-15 07:26:52 +0900

    checkbox作成
    st.checkbox("sample")

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from PIL import Image
+from PIL import Image      #画像を扱う
 
 st.title("Streamlit 超入門")
 
 st.write("Display Image")
 
-img = Image.open("IMGP3446.jpg")
-st.image(img, caption="Huyen", use_column_width=True)
+#チェックが入っていたらTrue(画像を表示)
+if st.checkbox("show Image"):
+    img = Image.open("IMGP3446.jpg")
+    st.image(img, caption="Huyen", use_column_width=True)     #use_column_width=Trueでアプリ、画像の幅に自動で合わせる
