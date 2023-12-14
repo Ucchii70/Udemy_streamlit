e06eb70d5313677b5010026942ccb34d8ad22f0a
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-15 08:28:24 +0900

    st.text_input
    文字を入力したら下のメッセージに反映

--- main.py
+++ main.py
@@ -7,13 +7,5 @@ st.title("Streamlit 超入門")
 
 st.write("Interactive Widgets")
 
-option = st.selectbox(
-    "あなたの好きな数字を教えてください。",
-    list(range(1,11))
-)
-"あなたの好きな数字は、", option, "です。"
-
-#チェックが入っていたらTrue(画像を表示)
-# if st.checkbox("show Image"):
-#     img = Image.open("IMGP3446.jpg")
-#     st.image(img, caption="Huyen", use_column_width=True)     #use_column_width=Trueでアプリ、画像の幅に自動で合わせる
+text = st.text_input("あなたの趣味を教えてください。")
+"あなたの好きな趣味は", text, "です。"
