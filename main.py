da2f7773954ed2c2345db68e72a93bf5036add2f
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-15 08:18:09 +0900

    checkbox,selectbox

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from PIL import Image      #画像を扱う
 
 st.title("Streamlit 超入門")
 
-st.write("Display Image")
+st.write("Interactive Widgets")
+
+option = st.selectbox(
+    "あなたの好きな数字を教えてください。",
+    list(range(1,11))
+)
+"あなたの好きな数字は、", option, "です。"
 
 #チェックが入っていたらTrue(画像を表示)
-if st.checkbox("show Image"):
-    img = Image.open("IMGP3446.jpg")
-    st.image(img, caption="Huyen", use_column_width=True)     #use_column_width=Trueでアプリ、画像の幅に自動で合わせる
+# if st.checkbox("show Image"):
+#     img = Image.open("IMGP3446.jpg")
+#     st.image(img, caption="Huyen", use_column_width=True)     #use_column_width=Trueでアプリ、画像の幅に自動で合わせる
