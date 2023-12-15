e914942e3a1e05728386bf06cec79875971290d5
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-16 07:31:25 +0900

    st.slider
    スライダーを描く

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ st.title("Streamlit 超入門")
 st.write("Interactive Widgets")
 
 text = st.text_input("あなたの趣味を教えてください。")
-"あなたの好きな趣味は", text, "です。"
+"あなたの趣味: ", text
+
+condistion = st.slider("あなたの今の調子は？", 0, 100, 50)
+"コンディション: ", condistion
