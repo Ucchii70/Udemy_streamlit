bf323bbaa7633242789b8fa8701ae62ea9f0896f
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-17 07:24:36 +0900

    st.columns
    ボタンにカラムを表示

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ st.title("Streamlit 超入門")
 
 st.write("Interactive Widgets")
 
-text = st.text_input("あなたの趣味を教えてください。")
-"あなたの趣味: ", text
+left_column, right_column = st.columns(2)
+button = left_column.button("右カラムに文字を表示")
+if button:
+    right_column.button("ここは右カラム")
 
-condistion = st.slider("あなたの今の調子は？", 0, 100, 50)
-"コンディション: ", condistion
+# #sidebarの追加
+# text = st.text_input("あなたの趣味を教えてください。")
+# condistion = st.slider("あなたの今の調子は？", 0, 100, 50) 
+
+# "あなたの趣味: ", text
+# "コンディション: ", condistion
