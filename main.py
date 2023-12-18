b44fa7cf4c9a8905e446e1e17215a33223809cf8
Ucchii70 <137304583+Ucchii70@users.noreply.github.com>
2023-12-18 18:48:08 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image       #画像を扱う
 import time
 
 st.title("Streamlit 超入門")
@@ -32,11 +29,3 @@ expander1.write("問い合わせ1の回答")
 
 expander2 = st.expander("問い合わせ2")
 expander2.write("問い合わせ2の回答")
-
-
-# #sidebarの追加
-# text = st.text_input("あなたの趣味を教えてください。")
-# condistion = st.slider("あなたの今の調子は？", 0, 100, 50) 
-
-# "あなたの趣味: ", text
-# "コンディション: ", condistion
